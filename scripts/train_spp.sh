b20ce2324c7917a58a7d96ce008e6b483264b69f
LiYuxian <u3629246@connect.hku.hk>
2025-12-04 15:23:48 +0800

    train_spp

--- scripts/train_spp.sh
+++ scripts/train_spp.sh
@@ -13,25 +13,25 @@
 #SBATCH --partition=submit  
 
 eval "$(conda shell.bash hook)"
-conda activate mask3d_2
 
 export OMP_NUM_THREADS=3  # speeds up MinkowskiEngine
 
 # print node name using hostname
 echo "Running on $(hostname)"
 
-python main_instance_segmentation.py \
-    general.save_root=/cluster/eriador/cyeshwanth/caption3d/mask3d/checkpoints \
+/disk1/work/kylin/anaconda3/envs/excap3d_env/bin/python main_instance_segmentation.py \
+    general.save_root=/home/kylin/lyx/project_study/ExCap3D/data/excap_checkpoint \
     data.train_dataset.dataset_name=scannetpp \
     data.validation_dataset.dataset_name=scannetpp \
     data.train_dataset.clip_points=300000 \
     data.ignore_label=-100 \
     general.segment_strategy="majority_instance"  \
-    data.data_dir=/cluster/eriador/cyeshwanth/caption3d/mask3d/spp_data_0.005_40_v2_alpha \
-    data.train_dataset.list_file=/cluster/eriador/cyeshwanth/scannetpp_download_v2_alpha/splits/nvs_sem_train.txt \
-    data.validation_dataset.list_file=/cluster/eriador/cyeshwanth/scannetpp_download_v2_alpha/splits/nvs_sem_val.txt \
-    data.semantic_classes_file=/cluster/eriador/cyeshwanth/scannetpp_download_v2/metadata/semantic_benchmark/top100.txt \
-    data.instance_classes_file=/cluster/eriador/cyeshwanth/scannetpp_download_v2_alpha/metadata/semantic_benchmark/top100_instance.txt caption_model.class_weights_file=null \
+    data.data_dir=/home/kylin/lyx/project_study/ExCap3D/data/excap3d_final/ \
+    data.train_dataset.list_file=/home/kylin/lyx/project_study/ExCap3D/code/excap3d/train_list.txt \
+    data.validation_dataset.list_file=/home/kylin/lyx/project_study/ExCap3D/code/excap3d/test_list.txt \
+    data.semantic_classes_file=/home/kylin/datasets/scannetpp/scannetpp/metadata/semantic_benchmark/top100.txt \
+    data.instance_classes_file=/home/kylin/datasets/scannetpp/scannetpp/metadata/semantic_benchmark/top100_instance.txt \
+    caption_model.class_weights_file=null \
     data.batch_size=6 \
     'general.wandb_group="train instance segmentation"' \
     'general.notes="train"' \
