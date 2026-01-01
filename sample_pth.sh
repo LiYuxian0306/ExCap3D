ce8d263c47f2cbcc5d7b877c446f6cde196788d8
LiYuxian <u3629246@connect.hku.hk>
2026-01-01 23:07:58 +0800

    change_path

--- sample_pth.sh
+++ sample_pth.sh
@@ -29,7 +29,7 @@ which python
     sequential=True \
     data_dir=/home/kylin/datasets/scannetpp_v2/scannetpp/data/ \
     input_pth_dir=/home/kylin/lyx/project_study/ExCap3D/data/semantic_processed/semantic_processed_unchunked/ \
-    list_path=/home/kylin/lyx/project_study/ExCap3D/code/scannetpp/semantic/configs/process.txt \
+    list_path=/home/kylin/lyx/project_study/ExCap3D/code/excap3d/scannetpp/semantic/configs/process.txt \
     segments_dir=/home/kylin/datasets/scannetpp_v2/scannetpp/data/ \
     output_pth_dir=/home/kylin/lyx/project_study/ExCap3D/data/sampled/ \
     sample_factor=0.6 \
