d9617b4d52d925940d356526ffe7c2ad3c966492
LiYuxian <u3629246@connect.hku.hk>
2026-01-05 15:01:02 +0800

    conf_debug

--- scripts/train_spp.sh
+++ scripts/train_spp.sh
@@ -15,7 +15,7 @@
 eval "$(conda shell.bash hook)"
 
 export OMP_NUM_THREADS=3  # speeds up MinkowskiEngine
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0
 
 
 # print node name using hostname
